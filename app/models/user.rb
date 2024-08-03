5720cc21df639cdc1487a0cc19adaf96549afdb9
semensid2020 <semensid2020@gmail.com>
2024-08-03 12:14:45 +0700

    gem annotate added
    1. добавлен гем annotate и его конфиги
    2. сгенерировано описание для ранее созданной модели User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  amount_of_tokens :integer
+#  capital          :decimal(10, 2)   default(10000.0)
+#  name             :string
+#  username         :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  telegram_id      :bigint
+#
 class User < ApplicationRecord
 end
